b3c3812655c47a5649290a60d491d36700dd9639
gponyashkin <silithust@gmail.com>
2023-05-07 21:36:14 +0300

    Добавил роль в модель, добавил еще пользователей

--- Source/users.py
+++ Source/users.py
@@ -4,7 +4,12 @@ from dataclasses import dataclass
 @dataclass
 class User:
     name: str
-    customer_id: int
+    user_id: int
+    role: str = 'customer'
 
 
+HermoineGranger = User("Hermoine Granger", 1)
 HarryPotter = User("Harry Potter", 2)
+RonWeasly = User("Ron Weasly", 3)
+AlbusDumbledore = User("Albus Dumbledore", 4)
+NevilleLongbottom = User("Neville Longbottom", 5)

555b905d004089844ec9d013632befdbdf2d970e
gponyashkin <silithust@gmail.com>
2023-05-07 19:16:57 +0300

    Добавил инфо о пользователе в отчет

--- UI/Tests/conftest.py
+++ UI/Tests/conftest.py
@@ -1,7 +1,12 @@
+import allure
 import pytest
 from Source.users import HarryPotter
 
 
 @pytest.fixture(scope='session')
+@allure.title("Пользователь")
 def user():
-    return HarryPotter
+    user = HarryPotter
+    allure.attach(str(user.__dict__), user.name)
+    return user
+
